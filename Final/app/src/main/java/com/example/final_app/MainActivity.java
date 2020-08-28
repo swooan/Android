c21ee82d62349708076fc14c42b61ba5d03b2513
swooan <67904532+swooan@users.noreply.github.com>
2020-08-28 16:24:37 +0900

    final_proj 08/26

--- Final/app/src/main/java/com/example/final_app/MainActivity.java
+++ Final/app/src/main/java/com/example/final_app/MainActivity.java
@@ -1,11 +1,8 @@
 package com.example.final_app;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
-
-import res.VPAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,8 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ViewPager vp = findViewById(R.id.viewpager);
-        VPAdapter adapter = new VPAdapter(getSupportFragmentManager());
-        vp.setAdapter(adapter);
+
     }
 }
